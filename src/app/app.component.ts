542ac5df66f8357f017fc5bde000bad0825266c0
jonmolinna <jmolina2624@gmail.com>
2024-07-30 09:02:13 -0500

    Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'comments_angular';
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HomeIconComponent } from '../icons/home-icon/home-icon.component';
+import { FireIconComponent } from '../icons/fire-icon/fire-icon.component';
+import { PlusIconComponent } from '../icons/plus-icon/plus-icon.component';
+import { UserCircleIconComponent } from '../icons/user-circle-icon/user-circle-icon.component';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    HomeIconComponent,
+    FireIconComponent,
+    PlusIconComponent,
+    UserCircleIconComponent,
+  ],
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {}
