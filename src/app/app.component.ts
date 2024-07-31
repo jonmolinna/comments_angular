afc0299d8364aefcd67fe1df056a36ae84fb69e5
jonmolinna <jmolina2624@gmail.com>
2024-07-31 09:58:11 -0500

    Add Search Suggestions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CardComponent } from './components/card/card.component';
+import { SearchSuggestionsComponent } from './components/search-suggestions/search-suggestions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, CardComponent],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    CardComponent,
+    SearchSuggestionsComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
---
+++ src/app/components/search-suggestions/search-suggestions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { GlassIconComponent } from '../icons/glass-icon/glass-icon.component';
+
+@Component({
+  selector: 'app-search-suggestions',
+  standalone: true,
+  imports: [GlassIconComponent],
+  templateUrl: './search-suggestions.component.html',
+})
+export class SearchSuggestionsComponent {}
